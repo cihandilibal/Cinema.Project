ac0bff38d3f941a04fd9d07056a5c4b8c02ffab8
cihan dilibal <dilibalcihan@gmail.com>
2024-09-26 17:37:11 +0300

    CONF yapildi, DAL'da Extensions yarım kaldi.DAL Repositoryleri ve context yapildi.appsettings.json yazildi.

--- Project.BLL/ServiceInjections/DbContextService.cs
+++ Project.BLL/ServiceInjections/DbContextService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Project.BLL.ServiceInjections
 {
-    internal class DbContextService
+    public class DbContextService
     {
     }
 }
--- Project.BLL/ServiceInjections/ManagerService.cs
+++ Project.BLL/ServiceInjections/ManagerService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Project.BLL.ServiceInjections
 {
-    internal class ManagerService
+    public class ManagerService
     {
     }
 }
